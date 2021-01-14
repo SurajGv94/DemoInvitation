d159534f13483e86fda50293f4ef56161515a938
SurajGv94 <77317197+SurajGv94@users.noreply.github.com>
2021-01-14 13:03:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,13 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
+
+Swal.fire({
+        title: "Annaberu Matt's <br> Wedding Invetation",
+        confirmButtonText: "Welcome",
+        
+      });
+
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
